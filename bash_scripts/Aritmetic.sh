7a26711425434380564d6fba80abb8777d7aa2d2
Daniel Gonzalez <danoid2007@gmail.com>
2022-07-21 11:07:07 +0200

    Add changes on Aritmetic.sh
    I have added more lines of code on the file Aritmetic.sh to show if the
    value of the number  you had inserted is between [5, 10].
    Furthermore, I have added at the line 15 a echo comand to separte the
    two parts of the code that makes the output easier to understand to the
    final user.

--- bash_scripts/Aritmetic.sh
+++ bash_scripts/Aritmetic.sh
@@ -11,3 +11,11 @@ elif [[ $inserted_value -lt 5 ]]; then  # -lt / lower than  -le / lower or equal
 elif [[ $inserted_value -eq 5 ]]; then  # -eq / equal than  -ne / not equal
     echo 'Your number is equal than 5'
 fi
+
+echo '----------------------'
+
+if [ $inserted_value -ge 5 -a $inserted_value -le 10 ]; then    # -a / and  -o / or
+    echo 'The value of your number is between [5, 10]'
+else
+    echo 'The value of your number is not between [5,10]'
+fi
